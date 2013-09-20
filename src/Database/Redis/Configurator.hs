018e8f5efbaa2e7db2d478eb45fe364bb11a8fce
Ozgun Ataman <ozgun.ataman@soostone.com>
2013-09-20 18:18:43 -0400

    Cast from integral seems to fix configurator

--- src/Database/Redis/Configurator.hs
+++ src/Database/Redis/Configurator.hs
@@ -19,7 +19,8 @@ import           Network.Socket
 getRedisConfig :: C.Config -> IO H.ConnectInfo
 getRedisConfig c = do
     host <- C.lookupDefault "localhost" c "host"
-    port <- (H.PortNumber . PortNum) <$> C.lookupDefault 6379 c "port"
+    p <- C.lookupDefault 6379 c "port"
+    let port = H.PortNumber . fromIntegral $ (p :: Int)
     n <- C.lookupDefault 3 c "max-conn"
     to <- fromIntegral <$> C.lookupDefault (15 :: Int) c "timeout"
     return $ H.defaultConnectInfo { H.connectHost = host
